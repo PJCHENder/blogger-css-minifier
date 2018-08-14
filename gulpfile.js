861a662baadb771ba581276e63a85214e1ad386b
pjchender <pjchender@gmail.com>
2018-08-14 12:10:33 +0800

    Refactor blogger css files

--- gulpfile.js
+++ gulpfile.js
@@ -23,49 +23,29 @@ gulp.task('browser-reload', ['move-html', 'sass', 'concat-js'], function () {
 //  將不同的 js 檔案合併在同一支當中
 gulp.task('concat-js', function () {
   return gulp.src([
-    'vendor/jquery-3.1.1.js',
-    'vendor/tether.min.js',
-    'vendor/bootstrap.min.js',
-    'vendor/lodash.min.js',
     'src/js/*.js'
   ])
-        .pipe(concat('main.js'))
-        .pipe(gulp.dest('./dist'))
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('./dist'))
 })
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function () {
-  return gulp.src('src/sass/*.scss')
-        .pipe(sass())
-        .pipe($.autoprefixer({
-          browsers: ['last 2 versions', 'ie >= 9']
-        }))
-        .pipe(gulp.dest('./dist'))
+  return gulp.src('src/sass/application.scss')
+    .pipe(sass())
+    .pipe($.autoprefixer({
+      browsers: ['last 2 versions', 'ie >= 9']
+    }))
+    .pipe(cleanCSS())
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest('./dist'))
 })
-
-//  clean-css
-gulp.task('clean-css', ['sass'], function () {
-  return gulp.src(['./vendor/*.css', './dist/*.css'])
-        .pipe(cleanCSS())
-        .pipe(concat('main.css'), {newLine: ''})
-        .pipe(gulp.dest('./dist'))
-})
-
-//  將 pug 轉成 HTML
-// gulp.task('pug-compile', function () {
-//   return gulp.src('src/views/*.pug')
-//         .pipe(pug({
-//           // Your options in here.
-//         }))
-//         .pipe(gulp.dest('./dist'))
-//         .pipe(browserSync.stream())
-// })
 
 //  Move HTML to dist folder
 gulp.task('move-html', function () {
   return gulp.src('src/views/*.html')
-        .pipe(gulp.dest('./dist'))
-        .pipe(browserSync.stream())
+    .pipe(gulp.dest('./dist'))
+    .pipe(browserSync.stream())
 })
 
 gulp.task('default', ['serve'])
